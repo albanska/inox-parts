2ca9aad58ae03262cc0244a0ea9af15305f70984
Alban Maloku <albanmaloku2@gmail.com>
2026-02-19 15:55:38 +0100

    Update VideoPlayer.tsx

--- app/components/home/VideoPlayer.tsx
+++ app/components/home/VideoPlayer.tsx
@@ -1,54 +1,18 @@
 "use client";
 
-import { motion } from "motion/react";
-import { useRef, useState } from "react";
+import Image from "next/image";
 
-function VideoPlayer({ isMuted }: any) {
-  const [isOnView, setIsOnView] = useState(false)
-  const vidRef = useRef<any>(null)
-
-  function stopVideo() {
-    if(isOnView && vidRef.current) {
-      vidRef.current.pause()
-      setIsOnView(false)
-    }
-  }
-
-  function playVideo() {
-    if(!vidRef) return
-    vidRef.current.play()
-    setIsOnView(true)
-  }
-
-
+function VideoPlayer() {
   return (
-    <motion.div
-      viewport={{ amount: 0.5 }}
-      onViewportEnter={playVideo}
-      onViewportLeave={stopVideo}
-      className="relative w-full h-svh"
-    >
-      <div className="video-container">
-        <video
-          ref={vidRef} 
-          width="100%"
-          height="auto"
-          muted={isMuted}
-          autoPlay
-          loop
-          poster="/vid/placeholder.jpg"
-          playsInline
-          style={{
-            objectFit: "cover", // This makes the video cover the container
-            width: "100%",
-            height: "100dvh", // Set your desired height
-          }}
-        >
-          <source src="/vid/INOX.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </motion.div>
+    <div className="relative w-full h-svh">
+      <Image
+        src="/vid/inoxpart-bg.jpeg"
+        alt="INOX Parts"
+        fill
+        priority
+        className="object-cover"
+      />
+    </div>
   );
 }
 
